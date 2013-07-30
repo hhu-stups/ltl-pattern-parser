0f43b6ec810723e7c7c4ceb1488b1023c3b66549
Philipp Kantner <philippkantner@gmx.de>
2013-07-30 12:13:18 +0200

    Finished scope replacements

--- src/de/prob/ltl/parser/prolog/scope/AfterScopeReplacer.java
+++ src/de/prob/ltl/parser/prolog/scope/AfterScopeReplacer.java
@@ -1,79 +1,105 @@
 package de.prob.ltl.parser.prolog.scope;
 
+import de.prob.prolog.term.CompoundPrologTerm;
 import de.prob.prolog.term.PrologTerm;
 
 public class AfterScopeReplacer extends ScopeReplacer {
+	/*
+		after q
+		G(!q) | !q U (q & <term>)
 
+		<term>
+		G(a) : G(a)
+		F(a) : F(a)
+		X(a) : X(a)
+		H(a) : a S (q & a)
+		O(a) : !q S a
+		Y(a) : !q & Y(a)
+
+		a U b : a U b
+		a R b : a R b
+		a W b : a W b
+		a S b : (a & !q) S b
+		a T b : b S (q & b | a & b)
+	 */
 	@Override
 	public PrologTerm scopeFormula(PrologTerm term) {
-		// TODO Auto-generated method stub
-		return null;
+		// G(!q) | !q U (q & <term>)
+		PrologTerm notQ = new CompoundPrologTerm("not", getQ());
+		PrologTerm never = new CompoundPrologTerm("globally", notQ);
+		PrologTerm until = new CompoundPrologTerm("until", notQ, new CompoundPrologTerm("and", getQ(), term));
+		return new CompoundPrologTerm("or", never, until);
 	}
 
 	@Override
 	public PrologTerm globally(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// G(a) : G(a)
+		return new CompoundPrologTerm("globally", a);
 	}
 
 	@Override
 	public PrologTerm finallyOp(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// F(a) : F(a)
+		return new CompoundPrologTerm("finally", a);
 	}
 
 	@Override
 	public PrologTerm next(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// X(a) : X(a)
+		return new CompoundPrologTerm("next", a);
 	}
 
 	@Override
 	public PrologTerm historically(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// H(a) : a S (q & a)
+		return new CompoundPrologTerm("since", a, new CompoundPrologTerm("and", getQ(), a));
 	}
 
 	@Override
 	public PrologTerm once(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// O(a) : !q S a
+		PrologTerm notQ = new CompoundPrologTerm("not", getQ());
+		return new CompoundPrologTerm("since", notQ, a);
 	}
 
 	@Override
 	public PrologTerm yesterday(PrologTerm a) {
-		// TODO Auto-generated method stub
-		return null;
+		// Y(a) : !q & Y(a)
+		PrologTerm notQ = new CompoundPrologTerm("not", getQ());
+		return new CompoundPrologTerm("and", notQ, new CompoundPrologTerm("yesterday", a));
 	}
 
 	@Override
 	public PrologTerm until(PrologTerm a, PrologTerm b) {
-		// TODO Auto-generated method stub
-		return null;
+		// a U b : a U b
+		return new CompoundPrologTerm("until", a, b);
 	}
 
 	@Override
 	public PrologTerm weakuntil(PrologTerm a, PrologTerm b) {
-		// TODO Auto-generated method stub
-		return null;
+		// a W b : a W b
+		return new CompoundPrologTerm("weakuntil", a, b);
 	}
 
 	@Override
 	public PrologTerm release(PrologTerm a, PrologTerm b) {
-		// TODO Auto-generated method stub
-		return null;
+		// a R b : a R b
+		return new CompoundPrologTerm("release", a, b);
 	}
 
 	@Override
 	public PrologTerm since(PrologTerm a, PrologTerm b) {
-		// TODO Auto-generated method stub
-		return null;
+		// a S b : (a & !q) S b
+		PrologTerm notQ = new CompoundPrologTerm("not", getQ());
+		return new CompoundPrologTerm("since", new CompoundPrologTerm("and", a, notQ), b);
 	}
 
 	@Override
 	public PrologTerm trigger(PrologTerm a, PrologTerm b) {
-		// TODO Auto-generated method stub
-		return null;
+		// a T b : b S (q & b | a & b)
+		PrologTerm qAndB = new CompoundPrologTerm("and", getQ(), b);
+		PrologTerm aAndB = new CompoundPrologTerm("and", a, b);
+		return new CompoundPrologTerm("since", b, new CompoundPrologTerm("or", qAndB, aAndB));
 	}
 
 }
